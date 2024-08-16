2207375de1231466b587fb6bf5746a5aa1664eb4
khushboo8252 <khushbooranjan8252@gmail.com>
2024-08-16 11:03:09 +0530

    some changes

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import connectDb from "./database/db.js";
 import cookieParser from "cookie-parser";
 import cloudinary from "cloudinary";
-
+import path from 'path';
 
 dotenv.config();
 
@@ -28,6 +28,14 @@ import pinRoutes from "./routes/pinRoutes.js";
 // using routes
 app.use("/api/user", userRoutes);
 app.use("/api/pin", pinRoutes);
+
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
+
+app.get("*",(req,res) =>{
+  res.sendFile(path.join(__dirname, "frontend","dist","index.html"));
+})
 
 
 
