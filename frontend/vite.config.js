8b301a83b46489a497119e041a2b1deb2b0653d5
khushboo8252 <khushbooranjan8252@gmail.com>
2024-08-18 19:07:17 +0530

    add

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    proxy:{
-      "/api":{
-        target:"http://localhost:5000",
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true, // optional, if you need to modify the origin of the request
       },
     },
+  },
+  build: {
+    outDir: "public", // Output directory for the build files
   },
 });
